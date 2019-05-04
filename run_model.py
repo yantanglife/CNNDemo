74879d53d002ee59fa29d00ead77f6026c30e1c3
yantang <yantang@buaa.edu.cn>
2019-05-04 11:39:07 +0800

    add get_time_diff().

--- run_model.py
+++ run_model.py
@@ -1,10 +1,17 @@
 import os
 import tensorflow as tf
 from argparse import ArgumentParser
+from time import time
+from datetime import timedelta
 
 from model.rnn_model import RNNConfig, RNN
 from model.cnn_model import CNNConfig, CNN
 from util.data_process import get_batch, get_train_valid_test_data
+
+
+def get_time_diff(time_start):
+    time_diff = time() - time_start
+    return timedelta(seconds=int(round(time_diff)))
 
 
 def feed_data(x_batch, y_batch, keep_prob):
@@ -44,35 +51,34 @@ def train():
     last_improved = 0  # 记录上一次提升批次
     require_improvement = 1000  # 如果超过1000轮未提升，提前结束训练
     flag = False
-
+    time_start = time()
     for epoch in range(config.num_epochs):
         print('Epoch:', epoch + 1)
-
         batch_train = get_batch(x_train, y_train, config.batch_size)
-        # print(batch_train)
         for x_batch, y_batch in batch_train:
-
             feed_dict = feed_data(x_batch, y_batch, config.dropout_keep_prob)
-            # print(y_batch)
+            # 每多少轮输出在训练集合验证集上的性能
             if total_batch % config.print_per_batch == 0:
                 feed_dict[model.keep_prob] = 1.0
                 loss_train, acc_train = sess.run([model.loss, model.acc], feed_dict=feed_dict)
                 loss_val, acc_val = evaluate(sess, x_val, y_val)
-
+                # 保存最好结果
                 if acc_val > best_acc_val:
                     best_acc_val = acc_val
                     last_improved = total_batch
                     saver.save(sess=sess, save_path=save_path)
+                    # 标记最好结果
                     improved_str = '*'
                 else:
                     improved_str = ''
+                time_diff = get_time_diff(time_start)
                 msg = 'Iter: {0:>6}, Train Loss: {1:>6.2}, Train Acc: {2:>7.2%},' \
-                      + ' Val Loss: {3:>6.2}, Val Acc: {4:>7.2%},  {5}'
-                print(msg.format(total_batch, loss_train, acc_train, loss_val, acc_val, improved_str))
+                      + ' Val Loss: {3:>6.2}, Val Acc: {4:>7.2%},  Time: {5} {6}'
+                print(msg.format(total_batch, loss_train, acc_train, loss_val, acc_val, time_diff, improved_str))
 
             sess.run(model.optim, feed_dict=feed_dict)  # 运行优化
             total_batch += 1
-
+            # 验证集正确率长期没有提升，提前结束训练
             if total_batch - last_improved > require_improvement:
                 print("No optimization for a long time, auto-stopping...")
                 flag = True
@@ -82,17 +88,20 @@ def train():
 
 
 def test():
+    time_start = time()
     sess = tf.Session()
     sess.run(tf.global_variables_initializer())
     saver = tf.train.Saver()
     saver.restore(sess=sess, save_path=save_path)
     print('Testing...')
     loss_test, acc_test = evaluate(sess, x_test, y_test)
-    msg = 'Test Loss: {0:>6.2}, Test Acc: {1:>7.2%}'
-    print(msg.format(loss_test, acc_test))
+    time_diff = get_time_diff(time_start)
+    msg = 'Test Loss: {0:>6.2}, Test Acc: {1:>7.2%}\nTime usage: {2}'
+    print(msg.format(loss_test, acc_test, time_diff))
 
 
 if __name__ == "__main__":
+    # 训练集、验证集、测试集
     x_train, x_val, x_test, y_train, y_val, y_test = get_train_valid_test_data()
     parser = ArgumentParser()
     parser.add_argument("--model", choices=["cnn", "rnn"], default="cnn", help="which model to use.")
@@ -108,6 +117,7 @@ if __name__ == "__main__":
         model_name = "rnn"
     save_dir = './checkpoints'
     save_subdir = os.path.join(save_dir, model_name)
+    # 模型路径
     save_path = os.path.join(save_dir, model_name, 'best_validation')
     if args.state in ["train", "both"]:
         train()
